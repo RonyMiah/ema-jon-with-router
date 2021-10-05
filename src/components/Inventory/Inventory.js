ba02c58a77d530e09c49ff8dc77540d40596756a
Rony Miah <ronymiah2121@gmail.com>
2021-10-05 06:40:21 -0600

    Ok i am sucess change remote url

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -5,6 +5,7 @@ const Inventory = () => {
         <div>
             <h2>This is Enventory </h2>
             <h2>This is Enventory Two </h2>
+            <h3>This is meny meny git link </h3>
         </div>
     );
 };
